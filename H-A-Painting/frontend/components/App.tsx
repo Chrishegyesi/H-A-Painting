5e4a5568f5f95ce9790952eb2404e97574c1c9a0
Chrishegyesi <bradshaw888@icloud.com>
2026-01-21 14:47:52 -0500

    Remove Installation, NeuroFile, Playground, and Support components

--- H-A-Painting/frontend/components/App.tsx
+++ H-A-Painting/frontend/components/App.tsx
@@ -3,9 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
-import Installation from './Installation';
-import Playground from './Playground';
-import Support from './Support';
 import Contact from './Contact';
 
 export default function App() {
@@ -16,9 +13,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/installation" element={<Installation />} />
-          <Route path="/playground" element={<Playground />} />
-          <Route path="/support" element={<Support />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
--- H-A-Painting/frontend/components/Navbar.tsx
+++ H-A-Painting/frontend/components/Navbar.tsx
@@ -7,9 +7,6 @@ export default function Navbar() {
     <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/installation">Installation</Link>
-      <Link to="/playground">Playground</Link>
-      <Link to="/support">Support</Link>
       <Link to="/contact">Contact</Link>
     </nav>
   );
